c08f77300e696576f6410df62b6ff21ae5ccf29a
KiranMantha <kiranv.mantha@gmail.com>
2017-04-21 21:07:49 +0530

    tried on local ip

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const http = require('http'),
-    port = 3000;
+    port = 3000,
+    request = require('request'),
+    proxy = 'http://localhost:'+ port;
 
-http.createServer((req, res)=>{
-    console.log(req);
-    
-   
-   // Send the response body as "Hello World"
-   res.end();
+http.createServer((req, res) => {
+    //console.log(req);
+
+    // Send the response body as "Hello World"
+    request({
+        'url': 'http://google.com/',
+        'proxy': proxy
+    }).pipe(res);
 
 }).listen(port)
-console.log('listening on port '+ port);
+console.log('listening on port ' + port);
